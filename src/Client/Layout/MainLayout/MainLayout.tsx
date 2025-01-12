bf256be0f13376fe74058a3d042fab265aa0ed86
Pham Thuan <phamminhthuan912@gmail.com>
2025-01-13 01:06:52 +0700

    13/1/25

--- src/Client/Layout/MainLayout/MainLayout.tsx
+++ src/Client/Layout/MainLayout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Footer from "src/Client/Components/Footer"
+import Header from "src/Client/Components/Header"
 
 export default function MainLayout() {
   return (
     <div>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
