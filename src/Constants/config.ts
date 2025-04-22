214115787529b6276684ab376ce83a36dfa32377
Pham Thuan <phamminhthuan912@gmail.com>
2025-04-22 18:53:47 +0700

    test server deploy

--- src/Constants/config.ts
+++ src/Constants/config.ts
@@ -1,7 +1,7 @@
 // const { VITE_API_SERVER } = import.meta.env
 
 export const config = {
-  baseURLClient: "https://techzone-api-5gpz.onrender.com",
+  baseURLClient: "https://techzoneapi.io.vn/",
   // baseURLClient: VITE_API_SERVER,
   maxSizeUploadImage: 1048576 // 1mb = 1.048.576 byte
 }
